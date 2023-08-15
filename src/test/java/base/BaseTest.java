a66ce2aadea1ce52f05cf3c4fa8a258b6fea1a0e
Dusko Pralica <61793907+chillhijo@users.noreply.github.com>
2023-08-15 22:34:18 +0200

    Set starting and quit browser

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,35 @@
 package base;
 
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.*;
+
 public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        driver = new ChromeDriver(options);
+        driver.get("https://www.saucedemo.com/");
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-extensions");
+    }
+
+    @AfterMethod
+    public void cleanUp() {
+        driver.quit();
+    }
+
+    @Test
+    public void startBrowserTest() {
+        BaseTest baseTest = new BaseTest();
+        baseTest.setUp();
+        baseTest.cleanUp();
+    }
 }
