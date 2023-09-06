b626f9ec6b081d5f89c567f9fd93f5164cbc7491
Dusko Pralica <61793907+chillhijo@users.noreply.github.com>
2023-09-06 15:47:35 +0200

    Added new verification home page method

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,13 +1,33 @@
 package pages;
 
 import base.PageBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
 
 public class HomePage extends PageBase {
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
+    public void verifyHomePage() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-    
+        String ActualUrl = driver.getCurrentUrl();
+        String ExpectedUrl = "https://www.saucedemo.com/inventory.html";
+        Assert.assertEquals(ActualUrl, ExpectedUrl);
+        System.out.println(ActualUrl);
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("title"))));
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("social_twitter"))));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("social_facebook"))));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("social_linkedin"))));
+
+    }
+
 }
