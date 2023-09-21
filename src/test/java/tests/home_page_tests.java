325b85c252077d6aeb852b86991164b7267720f6
Dusko Pralica <61793907+chillhijo@users.noreply.github.com>
2023-09-21 14:53:42 +0200

    Created new class for home page tests

---
+++ src/test/java/tests/home_page_tests.java
@@ -0,0 +1,10 @@
+package tests;
+
+import base.PageBase;
+import org.openqa.selenium.WebDriver;
+
+public class home_page_tests extends PageBase {
+    public home_page_tests(WebDriver driver) {
+        super(driver);
+    }
+}
