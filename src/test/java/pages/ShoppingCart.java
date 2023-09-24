3fb8ebcb89072f94410073f08fc0517d8742097e
Dusko Pralica <61793907+chillhijo@users.noreply.github.com>
2023-09-24 18:43:29 +0200

    created new shoppingCart class

---
+++ src/test/java/pages/ShoppingCart.java
@@ -0,0 +1,10 @@
+package pages;
+
+import base.PageBase;
+import org.openqa.selenium.WebDriver;
+
+public class ShoppingCart extends PageBase {
+    public ShoppingCart(WebDriver driver) {
+        super(driver);
+    }
+}
